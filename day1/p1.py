7b0dcabcb41c6e1459e31904df7256da45936a1f
chethanabm2010-wq <chethanabm2010@gmail.com>
2025-08-18 20:05:41 +0530

    day1

---
+++ day1/p1.py
@@ -0,0 +1,1 @@
+print(print("I love Python"))
---
+++ day1/p2.py
@@ -0,0 +1,10 @@
+# Check whether the given number is Even
+
+input_number=int(input("Enter the integer number :"))
+
+if input_number % 2 == 0:
+    print("It is an Even Number")
+else:
+    print("It is not an Even Number")
+    
+
